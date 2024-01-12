b279705fa6d5332da277151e0de89f36d4f34572
pchalotra <pradeep20032000@gmail.com>
2024-01-12 17:02:06 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
+const Footer = (props) => {
+  return (
+    <footer className='bg-dark'>
+      <div className="container py-3">
+        <div className="row text-light mt-3">
+        <div className="col-md">
+            <div className="d-flex align-items-center" style={{fontSize: '20px', fontWeight: '500'}}>
+              <img src={logo} alt="NewsApp" style={{width: '42px', marginRight: '10px'}} /> {props.brandName}
+            </div> 
+            <p>Get all the latest news at one place, just a touch away!</p>
+            {/* <p>Copyright &copy; 2023 NewsApp</p> */}
+            <p>Developed By <a className="pointer text-decoration-none text-light footer-link" href="https://pradeep/" target="_blank" rel="noreferrer">Pradeep Chalotra</a></p>
+          </div>
+          <div className="col-md-3 mt-4 mt-md-0">
+            <h4 style={{fontWeight: 'normal'}}>Quick Links</h4>
+            <Link className="nav-link footer-link my-1" to="/business">Business</Link>
+            <Link className="nav-link footer-link my-1" to="/entertainment">Entertainment</Link>
+            <Link className="nav-link footer-link my-1" to="/general">General</Link>
+            <Link className="nav-link footer-link my-1" to="/health">Health</Link>
+            <Link className="nav-link footer-link my-1" to="/science">Science</Link>
+            <Link className="nav-link footer-link my-1" to="/sports">Sports</Link>
+            <Link className="nav-link footer-link my-1" to="/technology">Technology</Link>
+          </div>
+
+        </div>
+          
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import { NavLink, Link } from 'react-router-dom'; // NavLink used for automatically adding 'active' class while nav-link is active
+
+ const NavBar = (props) => {
+  return (
+      <nav className="navbar fixed-top navbar-dark bg-dark navbar-expand-lg">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="NewsApp" style={{width: '42px', marginLeft: '2px'}} />
+        </Link>
+        <Link className="navbar-brand" to="/"> {props.brandName} </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Category
+              </Link>
+              <ul className="dropdown-menu bg-dark">
+                <li><NavLink className="nav-link" to="/business">Business</NavLink></li>
+                <li><NavLink className="nav-link" to="/entertainment">Entertainment</NavLink></li>
+                <li><NavLink className="nav-link" to="/general">General</NavLink></li>
+                <li><NavLink className="nav-link" to="/health">Health</NavLink></li>
+                <li><NavLink className="nav-link" to="/science">Science</NavLink></li>
+                <li><NavLink className="nav-link" to="/sports">Sports</NavLink></li>
+                <li><NavLink className="nav-link" to="/technology">Technology</NavLink></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
